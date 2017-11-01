8d374631894ddae14e37b95f2a84b6e02a80571e
B1 <B1@B144>
2017-11-01 13:14:07 +0800

    my first commit here

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -18,6 +18,7 @@ public class JavaApplication1 {
         // TODO code application logic here
         System.out.println("maji");
         System.out.println("da");
+        System.out.println("yo");
     }
     
 }
